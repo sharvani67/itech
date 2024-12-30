47e1b8a430573c1f072850885000dc93676ab5d3
unknown <sharvanikokkonda@gmail.com>
2024-12-30 16:29:21 +0530

    resolved RABC

--- itech_solutions/views.py
+++ itech_solutions/views.py
@@ -101,15 +101,20 @@ class deletesoftware(APIView):
 def contact(request):
     return render(request,'contact.html')
 
+ # Import Profile if you're using it
+
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-#from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile  # Import the UserProfile model
 
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('User', 'User'),
+]
 def register_view(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -124,6 +129,7 @@ def register_view(request):
             elif User.objects.filter(email=email).exists():
                 messages.error(request, "Email already exists")
             else:
+                # Create a new user
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
@@ -142,16 +148,31 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
+            # Log in the user
             login(request, user)
+
+            # Retrieve the user's profile and set session variables
+            user_profile = UserProfile.objects.get(user=user)
+            request.session['username'] = user.username  # Store username in session
+            request.session['role'] = user_profile.role  # Store role in session
+
+           
             return redirect('home')
+
         else:
             messages.error(request, "Invalid username or password")
     return render(request, 'authentication/login.html')
 
 
+@login_required
 def logout_view(request):
+    # Clear all session data
+    request.session.flush()
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect('login')
+
+
 
